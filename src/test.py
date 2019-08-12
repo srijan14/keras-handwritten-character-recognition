d27220513643130d76ed10292e42d1df18bb7cb5
srijan14 <srijansharma007@gmail.com>
2019-08-12 21:49:14 +0530

    using argsparse

--- src/test.py
+++ src/test.py
@@ -1,21 +1,18 @@
-import sys
 from src.model import Model
+import argparse
+import os
 
 if __name__=="__main__":
-    if len(sys.argv) !=2 :
-        print("Please provide required parameters. Example run\npython src/test.py  models/model.01-1.1955.hdf5 data/test/s.jpg")
 
-    if len(sys.argv) == 3:
-        model_path = sys.argv[1]
-        img_path = sys.argv[2]
-    elif len(sys.argv) == 2:
-        img_path = None
-        model_path = sys.argv[1]
-    else:
-        img_path = None
-        model_path = None
+    parser = argparse.ArgumentParser(description='Testing on models generated from train.py on the EMNIST dataset')
+    parser.add_argument('--model', type=str, default='./models/model.hdf5', help='Directory path containing the model')
+    parser.add_argument('--data', type=str, default='./data/test/s.jpg', help='path of testing file')
+    args = parser.parse_args()
 
+    if not os.path.exists(args.model) or not os.path.exists(args.data):
+        print("Invalid parameters provided")
+        exit()
 
     test_model = Model()
-    test_model.loadmodel(path=model_path)
-    test_model.test(img_path=img_path)
+    test_model.loadmodel(path=args.model)
+    test_model.test(img_path=args.data)
--- src/train.py
+++ src/train.py
@@ -1,12 +1,17 @@
-import sys
+import os
+import argparse
 from src.model import Model
 
 if __name__=="__main__":
-    if len(sys.argv) !=2 :
-        print("Please provide required parameters. Training data path not provided. Example Run \n python src/train.py data/emnist-byclass.mat")
+    parser = argparse.ArgumentParser(description='Training on the EMNIST dataset')
+    parser.add_argument('--data', type=str, default='./data/emnist-byclass.mat', help="Trainnig data pickle file path")
+    args = parser.parse_args()
+
+    if not os.path.exists(args.data):
+        print("Invalid training data path provided")
         exit()
-    data_path = sys.argv[1]
+
     train_model = Model()
-    train_model.load_data(data_path)
+    train_model.load_data(args.data)
     train_model.character_model()
     train_model.train()
