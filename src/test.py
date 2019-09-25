52b736f923a0634c9942736ac865b41776e08b86
srijan14 <srijansharma007@gmail.com>
2019-09-25 21:36:24 +0530

    updated test data

--- src/test.py
+++ src/test.py
@@ -6,7 +6,7 @@ if __name__=="__main__":
 
     parser = argparse.ArgumentParser(description='Testing on models generated from train.py on the EMNIST dataset')
     parser.add_argument('--model', type=str, default='./models/model.h5', help='Directory path containing the model')
-    parser.add_argument('--data', type=str, default="./data/test/test.png", help='path of testing file')
+    parser.add_argument('--data', type=str, default="./data/emnist-byclass.mat", help='path of testing file')
     args = parser.parse_args()
 
     if not os.path.exists(args.model) or not os.path.exists(args.data):
